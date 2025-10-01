a93917c50912fd78b766e1af5b5b82b03bebb7a4
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-10-01 09:57:21 -0400

    3.9 AddTorque – Apply Rotational Force to Objects
    Added rotating obstacles with AddTorque.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] float torqueAmount = 3f;
+    InputAction moveAction;
+    Rigidbody2D myRigidbody2D;
+    
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.x < 0)
+        {
+            myRigidbody2D.AddTorque(torqueAmount);
+        }
+        else if (moveVector.x > 0)
+        {
+            myRigidbody2D.AddTorque(-torqueAmount);
+        }
+    }
+}
