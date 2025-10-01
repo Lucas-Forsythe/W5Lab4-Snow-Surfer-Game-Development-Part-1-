6c77f9d28c72ec0efb106e6957ef7d39f9cd4a67
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-10-01 10:17:48 -0400

    3.11 NameSpace & SceneManagement
    Scene Management

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+        if (collision.gameObject.layer == layerIndex)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
